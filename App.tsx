9e63ccae042355e6729e1d7cd40617ac328d6a62
Luan Evangelista <luan.evieira43@gmail.com>
2022-09-01 10:22:09 -0300

    Ajustando o expo-splash-screen

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import AppLoading from 'expo-app-loading';
+import * as SplashScreen from 'expo-splash-screen';
 import { ThemeProvider } from 'styled-components';
 import {
   useFonts,
@@ -12,6 +12,7 @@ import theme from './src/global/styles/theme';
 import { Dashboard } from './src/screens/Dashboard';
 
 export default function App() {
+  SplashScreen.preventAutoHideAsync();
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
     Poppins_500Medium,
@@ -19,8 +20,10 @@ export default function App() {
   });
 
   if(!fontsLoaded){
-    return <AppLoading />
+    return null;
   }
+
+  SplashScreen.hideAsync();
 
   return (
     <ThemeProvider theme={theme}>
