d8b5e76da9cabcad482bad8daf0c07a28279de17
Luan Evangelista <luan.evieira43@gmail.com>
2022-09-02 10:09:20 -0300

    Add Header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
 
-import { Container, Title } from './styles';
+import {
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName
+} from './styles';
 
-export function Dashboard(){
-    return(
+export function Dashboard() {
+    return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo
+                            source={{ uri: 'https://avatars.githubusercontent.com/u/72029952?v=4' }}
+                        />
+                        <User>
+                            <UserGreeting>Olá, </UserGreeting>
+                            <UserName>Luan Evangelista</UserName>
+                        </User>
+                    </UserInfo>
+                </UserWrapper>
+            </Header>
         </Container>
     )
 }
