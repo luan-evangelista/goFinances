6f986354b886deab572c549a18768fca8b16df5a
Luan Evangelista <luan.evieira43@gmail.com>
2022-10-12 02:05:42 -0300

    Add Flatlist CategorySelect

--- src/screens/CategorySelect/index.tsx
+++ src/screens/CategorySelect/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { FlatList } from 'react-native';
+
+import { Button } from '../../components/Forms/Button'
+
+import { categories } from '../../utils/categories';
 
 import {
     Container,
     Header,
     Title,
+    Category,
+    Icon,
+    Name,
+    Separator,
+    Footer,
 } from './styles';
 
 interface Category {
@@ -27,6 +37,23 @@ export function CategorySelect({
             <Header>
                 <Title>Categoria</Title>
             </Header>
+
+            <FlatList 
+                data={categories}
+                style={{ flex: 1, width: '100%' }}
+                keyExtractor={(item) => item.key}
+                renderItem={({ item }) =>(
+                    <Category>
+                        <Icon name={item.icon} />
+                        <Name>{item.name}</Name>
+                    </Category>
+                )}
+                ItemSeparatorComponent={() => <Separator />}
+            />
+
+            <Footer>
+                <Button title="Selecionar" />
+            </Footer>
         </Container>
     );
 };
--- src/screens/CategorySelect/styles.ts
+++ src/screens/CategorySelect/styles.ts
@@ -1,4 +1,5 @@
 import { RFValue } from "react-native-responsive-fontsize";
+import { Feather } from '@expo/vector-icons';
 import styled from "styled-components/native";
 
 export const Container = styled.View`
@@ -21,4 +22,33 @@ export const Title = styled.Text`
     font-family: ${({ theme }) => theme.fonts.regular};
     color: ${({ theme }) => theme.colors.shape};
     font-size: ${RFValue(18)}px;
-`; 
+`;
+
+export const Category = styled.View`
+    width: 100%;
+    padding: ${RFValue(15)}px;
+
+    flex-direction: row;
+    align-items: center;
+`;
+
+export const Icon = styled(Feather)`
+    font-size: ${RFValue(20)}px;
+    margin-right: 16px;
+`;
+
+export const Name = styled.Text`
+    font-family: ${({ theme }) => theme.fonts.regular};
+    font-size: ${RFValue(14)}px;
+`;
+
+export const Separator = styled.View`
+    height: 1px;
+    width: 100%;
+    background-color: ${({ theme }) => theme.colors.text};
+`;
+
+export const Footer = styled.View`
+    width: 100%;
+    padding: 24px;
+`;
